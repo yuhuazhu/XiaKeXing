9d2eff888ea39539c20c9180c570d798ce0839f9
余华柱 <yuhuazhu@live.com>
2015-04-27 21:31:39 +0800

    y:测试

--- src/com/example/test/LeftActivity.java
+++ src/com/example/test/LeftActivity.java
@@ -11,7 +11,7 @@ public class LeftActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_left);
+		setContentView(R.layout.activity_left); 
 	}
 	
 	public void StartMain(View v){
