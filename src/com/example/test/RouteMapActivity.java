95a040b0606d6591b08dae7cfeb55dbe7b0c4a1e
余华柱 <yuhuazhu@live.com>
2015-05-02 09:53:49 +0800

    //添加地图语音播放功能    --  zlx

--- src/com/example/test/RouteMapActivity.java
+++ src/com/example/test/RouteMapActivity.java
@@ -1,6 +1,8 @@
 package com.example.test;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.app.Activity;
 import android.content.Context;
@@ -49,78 +51,74 @@ public class RouteMapActivity extends Activity implements OnClickListener {
 	private boolean openstate = false;
 	private ImageButton imgswitch;
 	private int soundID;
-	private final String[][] MIME_MapTable={ 
-            //{后缀名，MIME类型} 
-            {".3gp",    "video/3gpp"}, 
-            {".apk",    "application/vnd.android.package-archive"}, 
-            {".asf",    "video/x-ms-asf"}, 
-            {".avi",    "video/x-msvideo"}, 
-            {".bin",    "application/octet-stream"}, 
-            {".bmp",    "image/bmp"}, 
-            {".c",  "text/plain"}, 
-            {".class",  "application/octet-stream"}, 
-            {".conf",   "text/plain"}, 
-            {".cpp",    "text/plain"}, 
-            {".doc",    "application/msword"}, 
-            {".docx",   "application/vnd.openxmlformats-officedocument.wordprocessingml.document"}, 
-            {".xls",    "application/vnd.ms-excel"},  
-            {".xlsx",   "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"}, 
-            {".exe",    "application/octet-stream"}, 
-            {".gif",    "image/gif"}, 
-            {".gtar",   "application/x-gtar"}, 
-            {".gz", "application/x-gzip"}, 
-            {".h",  "text/plain"}, 
-            {".htm",    "text/html"}, 
-            {".html",   "text/html"}, 
-            {".jar",    "application/java-archive"}, 
-            {".java",   "text/plain"}, 
-            {".jpeg",   "image/jpeg"}, 
-            {".jpg",    "image/jpeg"}, 
-            {".js", "application/x-javascript"}, 
-            {".log",    "text/plain"}, 
-            {".m3u",    "audio/x-mpegurl"}, 
-            {".m4a",    "audio/mp4a-latm"}, 
-            {".m4b",    "audio/mp4a-latm"}, 
-            {".m4p",    "audio/mp4a-latm"}, 
-            {".m4u",    "video/vnd.mpegurl"}, 
-            {".m4v",    "video/x-m4v"},  
-            {".mov",    "video/quicktime"}, 
-            {".mp2",    "audio/x-mpeg"}, 
-            {".mp3",    "audio/x-mpeg"}, 
-            {".mp4",    "video/mp4"}, 
-            {".mpc",    "application/vnd.mpohun.certificate"},        
-            {".mpe",    "video/mpeg"},   
-            {".mpeg",   "video/mpeg"},   
-            {".mpg",    "video/mpeg"},   
-            {".mpg4",   "video/mp4"},    
-            {".mpga",   "audio/mpeg"}, 
-            {".msg",    "application/vnd.ms-outlook"}, 
-            {".ogg",    "audio/ogg"}, 
-            {".pdf",    "application/pdf"}, 
-            {".png",    "image/png"}, 
-            {".pps",    "application/vnd.ms-powerpoint"}, 
-            {".ppt",    "application/vnd.ms-powerpoint"}, 
-            {".pptx",   "application/vnd.openxmlformats-officedocument.presentationml.presentation"}, 
-            {".prop",   "text/plain"}, 
-            {".rc", "text/plain"}, 
-            {".rmvb",   "audio/x-pn-realaudio"}, 
-            {".rtf",    "application/rtf"}, 
-            {".sh", "text/plain"}, 
-            {".tar",    "application/x-tar"},    
-            {".tgz",    "application/x-compressed"},  
-            {".txt",    "text/plain"}, 
-            {".wav",    "audio/x-wav"}, 
-            {".wma",    "audio/x-ms-wma"}, 
-            {".wmv",    "audio/x-ms-wmv"}, 
-            {".wps",    "application/vnd.ms-works"}, 
-            {".xml",    "text/plain"}, 
-            {".z",  "application/x-compress"}, 
-            {".zip",    "application/x-zip-compressed"}, 
-            {"",        "*/*"}   
-        }; 
+	private final String[][] MIME_MapTable = {
+			// {后缀名，MIME类型}
+			{ ".3gp", "video/3gpp" },
+			{ ".apk", "application/vnd.android.package-archive" },
+			{ ".asf", "video/x-ms-asf" },
+			{ ".avi", "video/x-msvideo" },
+			{ ".bin", "application/octet-stream" },
+			{ ".bmp", "image/bmp" },
+			{ ".c", "text/plain" },
+			{ ".class", "application/octet-stream" },
+			{ ".conf", "text/plain" },
+			{ ".cpp", "text/plain" },
+			{ ".doc", "application/msword" },
+			{ ".docx",
+					"application/vnd.openxmlformats-officedocument.wordprocessingml.document" },
+			{ ".xls", "application/vnd.ms-excel" },
+			{ ".xlsx",
+					"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" },
+			{ ".exe", "application/octet-stream" },
+			{ ".gif", "image/gif" },
+			{ ".gtar", "application/x-gtar" },
+			{ ".gz", "application/x-gzip" },
+			{ ".h", "text/plain" },
+			{ ".htm", "text/html" },
+			{ ".html", "text/html" },
+			{ ".jar", "application/java-archive" },
+			{ ".java", "text/plain" },
+			{ ".jpeg", "image/jpeg" },
+			{ ".jpg", "image/jpeg" },
+			{ ".js", "application/x-javascript" },
+			{ ".log", "text/plain" },
+			{ ".m3u", "audio/x-mpegurl" },
+			{ ".m4a", "audio/mp4a-latm" },
+			{ ".m4b", "audio/mp4a-latm" },
+			{ ".m4p", "audio/mp4a-latm" },
+			{ ".m4u", "video/vnd.mpegurl" },
+			{ ".m4v", "video/x-m4v" },
+			{ ".mov", "video/quicktime" },
+			{ ".mp2", "audio/x-mpeg" },
+			{ ".mp3", "audio/x-mpeg" },
+			{ ".mp4", "video/mp4" },
+			{ ".mpc", "application/vnd.mpohun.certificate" },
+			{ ".mpe", "video/mpeg" },
+			{ ".mpeg", "video/mpeg" },
+			{ ".mpg", "video/mpeg" },
+			{ ".mpg4", "video/mp4" },
+			{ ".mpga", "audio/mpeg" },
+			{ ".msg", "application/vnd.ms-outlook" },
+			{ ".ogg", "audio/ogg" },
+			{ ".pdf", "application/pdf" },
+			{ ".png", "image/png" },
+			{ ".pps", "application/vnd.ms-powerpoint" },
+			{ ".ppt", "application/vnd.ms-powerpoint" },
+			{ ".pptx",
+					"application/vnd.openxmlformats-officedocument.presentationml.presentation" },
+			{ ".prop", "text/plain" }, { ".rc", "text/plain" },
+			{ ".rmvb", "audio/x-pn-realaudio" }, { ".rtf", "application/rtf" },
+			{ ".sh", "text/plain" }, { ".tar", "application/x-tar" },
+			{ ".tgz", "application/x-compressed" }, { ".txt", "text/plain" },
+			{ ".wav", "audio/x-wav" }, { ".wma", "audio/x-ms-wma" },
+			{ ".wmv", "audio/x-ms-wmv" },
+			{ ".wps", "application/vnd.ms-works" }, { ".xml", "text/plain" },
+			{ ".z", "application/x-compress" },
+			{ ".zip", "application/x-zip-compressed" }, { "", "*/*" } };
 
 	SoundPool mSoundPool = null;
-	
+	private Map<Integer, Integer> soundMap;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -131,27 +129,56 @@ public class RouteMapActivity extends Activity implements OnClickListener {
 		vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 
 		initUI();
-		mSoundPool= new SoundPool(10, AudioManager.STREAM_SYSTEM, 5);
+		mSoundPool = new SoundPool(10, AudioManager.STREAM_SYSTEM, 5);
+
+		soundMap = new HashMap<Integer, Integer>();
+
 		new Thread(new Runnable() {
 
 			@Override
 			public void run() {
-				//try {
-					soundID = mSoundPool.load(RouteMapActivity.this, R.raw.yindao, 1);
-					//Thread.sleep(10000);
-//				} catch (InterruptedException e) {
-//					e.printStackTrace();
-//				}
+				// try {
+				soundMap.put(1,
+						mSoundPool.load(RouteMapActivity.this, R.raw.yindao, 1));
+				soundMap.put(2,
+						mSoundPool.load(RouteMapActivity.this, R.raw.zizhu, 1));
+				soundMap.put(3, mSoundPool.load(RouteMapActivity.this,
+						R.raw.tiyan3D, 1));
+				soundMap.put(4,
+						mSoundPool.load(RouteMapActivity.this, R.raw.yyzs, 1));
+				soundMap.put(5,
+						mSoundPool.load(RouteMapActivity.this, R.raw.jiedai, 1));
+				soundMap.put(6,
+						mSoundPool.load(RouteMapActivity.this, R.raw.anmo, 1));
+				soundMap.put(7, mSoundPool.load(RouteMapActivity.this,
+						R.raw.playscreen, 1));
+				soundMap.put(8, mSoundPool.load(RouteMapActivity.this,
+						R.raw.xinglijicun, 1));
+				soundMap.put(9,
+						mSoundPool.load(RouteMapActivity.this, R.raw.yiwu, 1));
+				soundMap.put(10, mSoundPool.load(RouteMapActivity.this,
+						R.raw.banshouli, 1));
+				soundMap.put(11, mSoundPool.load(RouteMapActivity.this,
+						R.raw.duogongneng, 1));
+				soundMap.put(12,
+						mSoundPool.load(RouteMapActivity.this, R.raw.jifang, 1));
+				soundMap.put(13,
+						mSoundPool.load(RouteMapActivity.this, R.raw.yjzh, 1));
+				soundMap.put(14, mSoundPool.load(RouteMapActivity.this,
+						R.raw.bangongqu, 1));
+
+				// Thread.sleep(10000);
+				// } catch (InterruptedException e) {
+				// e.printStackTrace();
+				// }
 				// TODO Auto-generated method stub
-				
+
 			}
 		}).start();
-				
-				// TODO Auto-generated method stub
-				
-			
-		//设置最多可容纳10个音频流，音频的品质为5
-		
+
+		// TODO Auto-generated method stub
+
+		// 设置最多可容纳10个音频流，音频的品质为5
 
 	}
 
@@ -240,12 +267,18 @@ public class RouteMapActivity extends Activity implements OnClickListener {
 		if (sensorManager != null) {// 取消监听器
 			sensorManager.unregisterListener(sensorEventListener);
 		}
+		for (int i = 0; i < soundMap.size(); i++) {
+			mSoundPool.unload(soundMap.get(i + 1));
+
+		}
+
 		// TODO 注销广播
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
+
 		// if (sensorManager != null) {// 注册监听器
 		// sensorManager.registerListener(sensorEventListener,
 		// sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
@@ -322,11 +355,14 @@ public class RouteMapActivity extends Activity implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
-		AudioManager mgr = (AudioManager)this.getSystemService(Context.AUDIO_SERVICE);  
-		
-		float streamVolumeCurrent = mgr.getStreamVolume(AudioManager.STREAM_MUSIC);  
-		float streamVolumeMax = mgr.getStreamMaxVolume(AudioManager.STREAM_MUSIC);      
-		float volume = streamVolumeCurrent/streamVolumeMax;  
+		AudioManager mgr = (AudioManager) this
+				.getSystemService(Context.AUDIO_SERVICE);
+
+		float streamVolumeCurrent = mgr
+				.getStreamVolume(AudioManager.STREAM_MUSIC);
+		float streamVolumeMax = mgr
+				.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+		float volume = streamVolumeCurrent / streamVolumeMax;
 		int streamID1 = 0;
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
@@ -374,23 +410,22 @@ public class RouteMapActivity extends Activity implements OnClickListener {
 			if (playstate) {
 				// 暂停语音
 				imgplay.setBackgroundResource(R.drawable.ic_play);
-				//mSoundPool.unload(soundID);
-				mSoundPool.pause(soundID);
+
+				mSoundPool.pause(streamID1);
 				playstate = false;
 			} else {
 				// 播放语音
 				imgplay.setBackgroundResource(R.drawable.ic_pause);
-				
-				
-				streamID1 = mSoundPool.play(soundID, 1, 1, 1, 0, 1.0f);
-//				Intent it = new Intent(Intent.ACTION_VIEW);
-//				File file = new File("file:///android_asset/1yindao.m4a");
-//				//获取文件file的MIME类型 
-//			    String type = getMIMEType(file); 
-//				it.setDataAndType(
-//						Uri.fromFile(file),
-//						type);
-//				startActivity(it);
+
+				streamID1 = mSoundPool.play(soundMap.get(1), 1, 1, 1, 0, 1.0f);
+				// Intent it = new Intent(Intent.ACTION_VIEW);
+				// File file = new File("file:///android_asset/1yindao.m4a");
+				// //获取文件file的MIME类型
+				// String type = getMIMEType(file);
+				// it.setDataAndType(
+				// Uri.fromFile(file),
+				// type);
+				// startActivity(it);
 				playstate = true;
 			}
 			break;
@@ -398,28 +433,30 @@ public class RouteMapActivity extends Activity implements OnClickListener {
 			break;
 		}
 	}
-	
+
 	/**
 	 * 根据文件后缀名获得对应的MIME类型。
+	 * 
 	 * @param file
-	 */ 
-	private String getMIMEType(File file) { 
-	     
-	    String type="*/*"; 
-	    String fName = file.getName(); 
-	    //获取后缀名前的分隔符"."在fName中的位置。 
-	    int dotIndex = fName.lastIndexOf("."); 
-	    if(dotIndex < 0){ 
-	        return type; 
-	    } 
-	    /* 获取文件的后缀名*/ 
-	    String end=fName.substring(dotIndex,fName.length()).toLowerCase(); 
-	    if(end=="")return type; 
-	    //在MIME和文件类型的匹配表中找到对应的MIME类型。 
-	    for(int i=0;i<MIME_MapTable.length;i++){ //MIME_MapTable??在这里你一定有疑问，这个MIME_MapTable是什么？ 
-	        if(end.equals(MIME_MapTable[i][0])) 
-	            type = MIME_MapTable[i][1]; 
-	    }        
-	    return type; 
-	} 
+	 */
+	private String getMIMEType(File file) {
+
+		String type = "*/*";
+		String fName = file.getName();
+		// 获取后缀名前的分隔符"."在fName中的位置。
+		int dotIndex = fName.lastIndexOf(".");
+		if (dotIndex < 0) {
+			return type;
+		}
+		/* 获取文件的后缀名 */
+		String end = fName.substring(dotIndex, fName.length()).toLowerCase();
+		if (end == "")
+			return type;
+		// 在MIME和文件类型的匹配表中找到对应的MIME类型。
+		for (int i = 0; i < MIME_MapTable.length; i++) { // MIME_MapTable??在这里你一定有疑问，这个MIME_MapTable是什么？
+			if (end.equals(MIME_MapTable[i][0]))
+				type = MIME_MapTable[i][1];
+		}
+		return type;
+	}
 }
