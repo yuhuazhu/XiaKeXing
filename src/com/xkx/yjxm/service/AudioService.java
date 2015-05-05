12138e4f8e611e22b06facedf498ffeb4c9f9846
余华柱 <yuhuazhu@live.com>
2015-05-05 16:40:25 +0800

    y:add 注释

--- src/com/xkx/yjxm/service/AudioService.java
+++ src/com/xkx/yjxm/service/AudioService.java
@@ -10,12 +10,12 @@ import android.media.MediaPlayer.OnPreparedListener;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.IBinder;
-import android.util.Log;
 
 public class AudioService extends Service {
 	private MediaPlayer mediaPlayer;
-	//是不是已经导入了资源
+	// 是不是已经导入了资源
 	private boolean isFrist;
+
 	public AudioService() {
 	}
 
@@ -31,12 +31,12 @@ public class AudioService extends Service {
 			mediaPlayer.setDataSource(this, uri);
 			mediaPlayer.prepareAsync();
 			mediaPlayer.setOnPreparedListener(new OnPreparedListener() {
-				
+
 				@Override
 				public void onPrepared(MediaPlayer mp) {
 					mediaPlayer.start();
 					isFrist = true;
-					
+
 				}
 			});
 		} catch (IllegalArgumentException e) {
@@ -49,41 +49,53 @@ public class AudioService extends Service {
 			e.printStackTrace();
 		}
 		mediaPlayer.setOnCompletionListener(new OnCompletionListener() {
-			
+
 			@Override
 			public void onCompletion(MediaPlayer mp) {
-				//播完后要做的事
+				// 播完后要做的事
 			}
 		});
 	}
 
-
 	public class AudioBinder extends Binder {
-
+		// 初始化并播放
 		public void audioPlay(Uri uri) {
 			play(uri);
 		}
 
+		// 开始播放
 		public void audioStart() {
 			mediaPlayer.start();
 		}
+
+		// 暂停播放
 		public void audioPause() {
 			mediaPlayer.pause();
 			// 设置为true后，暂停服务后不会被kill掉
 			stopForeground(true);
 		}
+
+		// 停止播放
+		public void audioStop() {
+			mediaPlayer.stop();
+		}
+
+		// 是否在播放
 		public boolean audioIsPlaying() {
 			return mediaPlayer.isPlaying();
 		}
+
+		// 是否已经导入资源
 		public boolean audioIsFrist() {
 			return isFrist;
 		}
-		
+
+		// 播放下一个
 		public void audioPlayNext(Uri uri) {
 			isFrist = false;
 			play(uri);
 		}
-		
+
 	}
 
 	@Override
