c572d3ace0e9aae4eebe037ff2d1c4ae972010fd
余华柱 <yuhuazhu@live.com>
2015-05-06 21:38:12 +0800

    y:添加提示动画

--- src/com/xkx/yjxm/activity/ReflexActivity.java
+++ src/com/xkx/yjxm/activity/ReflexActivity.java
@@ -8,13 +8,21 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 
 //映像
 public class ReflexActivity extends Activity {
@@ -23,12 +31,14 @@ public class ReflexActivity extends Activity {
 	private ArrayList<Integer> m_ImgIdY = new ArrayList<Integer>();
 	private ViewPager pager;
 	private int attractionsCount = 3;		//景点数
+	private ImageView m_imageView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//移除ActionBar，在setContent之前调用下面这句，保证没有ActionBar
 		requestWindowFeature(Window.FEATURE_NO_TITLE);  
 		setContentView(R.layout.activity_reflex);
+		m_imageView = (ImageView) findViewById(R.id.imageView_tip);
 		pager = (ViewPager) findViewById(R.id.viewpager);
 		pager.setAdapter(new ViewPagerAdapter());
 		for (int i = 0; i < 3; i++) {
@@ -42,7 +52,39 @@ public class ReflexActivity extends Activity {
 			imageView.setBackgroundResource(m_ImgIdY.get(i));
 			m_ViewList.add(view);
 		}
+		StartTip();
 	}
+	
+	// 动画 
+		public void StartTip() {
+			DisplayMetrics dm = new DisplayMetrics();
+			getWindowManager().getDefaultDisplay().getMetrics(dm);
+			int iWidth  = dm.widthPixels;
+			int iHeight = dm.heightPixels;
+			TranslateAnimation animation = new TranslateAnimation(iWidth / 5, 0 - iWidth / 5, 0, 0);
+			animation.setDuration(3000);
+			m_imageView.startAnimation(animation);
+			animation.setAnimationListener(new AnimationListener() {
+				
+				@Override
+				public void onAnimationStart(Animation animation) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void onAnimationRepeat(Animation animation) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void onAnimationEnd(Animation animation) {
+					m_imageView.setVisibility(View.INVISIBLE);
+					
+				}
+			});
+		}
 	
 	class ViewPagerAdapter extends PagerAdapter{
 
@@ -72,9 +114,14 @@ public class ReflexActivity extends Activity {
 		
 	}
 	
+	
 	//返回
     public void Back(View v){
     	finish();
+    }
+    //隐藏
+    public void hide(View v){
+    	v.setVisibility(View.GONE);
     }
 
 	@Override
