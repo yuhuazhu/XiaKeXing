98a1c66afd449e87846721abeae1fecce66776f5
余华柱 <yuhuazhu@live.com>
2015-05-06 12:15:47 +0800

    //修改界面  --  zlx

--- src/com/xkx/yjxm/activity/ActivitiesActivity.java
+++ src/com/xkx/yjxm/activity/ActivitiesActivity.java
@@ -4,24 +4,45 @@ import com.xkx.yjxm.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 //活动资讯
 public class ActivitiesActivity extends Activity {
 
+	private RelativeLayout img_newprint;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//移除ActionBar，在setContent之前调用下面这句，保证没有ActionBar
-		requestWindowFeature(Window.FEATURE_NO_TITLE);  
+		// 移除ActionBar，在setContent之前调用下面这句，保证没有ActionBar
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_activities);
+		initUI();
+
+	}
+
+	public void gophotoview(View v) {
+
+		Intent intent = null;
+
+		intent = new Intent(this, PhotoWashActivity.class);
+
+		startActivity(intent);
+	}
+
+	private void initUI() {
+
+		img_newprint = (RelativeLayout) findViewById(R.id.print);
+
 		TextView textView = (TextView) findViewById(R.id.textView1);
 		textView.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				finish();
