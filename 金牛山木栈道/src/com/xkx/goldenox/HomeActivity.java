e1ce833376cd24637e1b8c1777105d318c81b90c
余华柱 <yuhuazhu@live.com>
2015-05-07 17:00:19 +0800

    主页，zhengtianbao,2015年5月7日17:00:56

---
+++ 金牛山木栈道/src/com/xkx/goldenox/HomeActivity.java
@@ -0,0 +1,24 @@
+package com.xkx.goldenox;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+public class HomeActivity extends FragmentActivity {
+
+	private final String TAG_FRAG_MENU = "com.xkx.goldenox.menu";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		FragmentManager fragMgr = getSupportFragmentManager();
+		FragmentTransaction fragTrans = fragMgr.beginTransaction();
+		// TODO Ìæ»»frament
+		fragTrans.replace(R.id.menu_frame, new Fragment(), TAG_FRAG_MENU);
+		fragTrans.commit();
+	}
+}
