bf337e5bffc7b48d60708c451082482b6f4f6de6
余华柱 <yuhuazhu@live.com>
2015-05-07 19:39:28 +0800

    //修改界面  --  zlx

--- 金牛山木栈道/src/com/xkx/goldenox/HomeActivity.java
+++ 金牛山木栈道/src/com/xkx/goldenox/HomeActivity.java
@@ -1,24 +1,26 @@
 package com.xkx.goldenox;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.view.SurfaceView;
-import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+
+import com.xkx.goldenox.utils.Utils;
 
 public class HomeActivity extends FragmentActivity {
 
 	private final String TAG_FRAG_MENU = "com.xkx.goldenox.menu";
     
-    
+    private RelativeLayout toplay;
+    private FrameLayout suface_frame;
+    private FrameLayout menu_frame;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
-
+        initUI();
 		FragmentManager fragMgr = getSupportFragmentManager();
 		FragmentTransaction fragTrans = fragMgr.beginTransaction();
 		// TODO Ìæ»»frament
@@ -27,6 +29,25 @@ public class HomeActivity extends FragmentActivity {
 
 		fragTrans.replace(R.id.menu_frame, new BottomFragment(), TAG_FRAG_MENU);
 		fragTrans.commit();
+		
+		
+		
+	}
+	private void initUI()
+	{
+		toplay = (RelativeLayout) findViewById(R.id.toplay);
+		RelativeLayout.LayoutParams param = new RelativeLayout.LayoutParams(Utils.px2dip(1080),Utils.px2dip(100));
+		
+		toplay.setLayoutParams(param);
+		
+		suface_frame = (FrameLayout) findViewById(R.id.suface_frame);
+		FrameLayout.LayoutParams param2 = new FrameLayout.LayoutParams(Utils.px2dip(1080),Utils.px2dip(507));
+	
+		suface_frame.setLayoutParams(param2);
+		
+		menu_frame = (FrameLayout) findViewById(R.id.menu_frame);
+		FrameLayout.LayoutParams param3 = new FrameLayout.LayoutParams(Utils.px2dip(1080),Utils.px2dip(1313));
+		menu_frame.setLayoutParams(param3);
 		
 		
 		
