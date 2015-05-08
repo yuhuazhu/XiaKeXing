47310b5027a422f8e2163542968711874541852f
余华柱 <yuhuazhu@live.com>
2015-05-08 08:50:17 +0800

    //修改界面  --  zlx

--- 金牛山木栈道/src/com/xkx/goldenox/HomeActivity.java
+++ 金牛山木栈道/src/com/xkx/goldenox/HomeActivity.java
@@ -1,13 +1,21 @@
 package com.xkx.goldenox;
 
+import java.io.File;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.widget.EditText;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
@@ -19,7 +27,14 @@ public class HomeActivity extends FragmentActivity {
 	private final String TAG_FRAG_MENU = "com.xkx.goldenox.menu";
 
 	private RelativeLayout toplay;
-	private FrameLayout suface_frame;
+	private GifView GifView1;
+
+	private MediaPlayer mediaPlayer;
+	private String filename;
+	private SurfaceView surfaceView;
+	private LinearLayout sufacelay;
+	private final static String TAG = "VodeoPlayActivity";
+	private int prosition = 0;
 	private FrameLayout menu_frame;
 
 	@Override
@@ -30,7 +45,7 @@ public class HomeActivity extends FragmentActivity {
 		FragmentManager fragMgr = getSupportFragmentManager();
 		FragmentTransaction fragTrans = fragMgr.beginTransaction();
 		// TODO 替换frament
-		fragTrans.replace(R.id.suface_frame, new VideoFragment(), "");
+		
 
 		fragTrans.replace(R.id.menu_frame, new BottomFragment(), "");
 		fragTrans.commit();
@@ -45,6 +60,21 @@ public class HomeActivity extends FragmentActivity {
 		return false;
 	}
 
+//	private void processPlay(int id, boolean play) {
+//		Uri uri = getUri(id);
+//		if (uri == null) {
+//			CrashHandler.getInstance().logToFile(Thread.currentThread(),
+//					new Exception("Uri null"));
+//			return;
+//		}
+//		updateHead(id, play);
+//		hasProcessedMap.put(id, true);
+//		isPlaying = true;
+//		soundlay.setVisibility(View.VISIBLE);
+//		audioBinder.audioPlay(uri);
+//	}
+	
+	
 	private void initUI() {
 		toplay = (RelativeLayout) findViewById(R.id.toplay);
 		RelativeLayout.LayoutParams param = new RelativeLayout.LayoutParams(
@@ -52,12 +82,12 @@ public class HomeActivity extends FragmentActivity {
 
 		toplay.setLayoutParams(param);
 
-		suface_frame = (FrameLayout) findViewById(R.id.suface_frame);
+		sufacelay = (LinearLayout) findViewById(R.id.sufacelay);
 
 		RelativeLayout.LayoutParams param2 = new RelativeLayout.LayoutParams(
 				Utils.px2dip(1080), Utils.px2dip(507));
 
-		suface_frame.setLayoutParams(param2);
+		sufacelay.setLayoutParams(param2);
 
 		menu_frame = (FrameLayout) findViewById(R.id.menu_frame);
 
@@ -65,6 +95,105 @@ public class HomeActivity extends FragmentActivity {
 				Utils.px2dip(1080), Utils.px2dip(1313));
 
 		menu_frame.setLayoutParams(param3);
+		surfaceView = (SurfaceView) findViewById(R.id.surfaceview);
+		surfaceView.getHolder().setFixedSize(176, 144);// 设置分辨率
+		surfaceView.getHolder()
+				.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);// 设置surfaceview不维护自己的缓冲区，而是等待屏幕的渲染引擎将内容推送到用户面前
+		surfaceView.getHolder().addCallback(new SurceCallBack());// 对surface对象的状态进行监听
+		mediaPlayer = new MediaPlayer();
 
+		ButtonOnClikListiner buttonOnClikListinero = new ButtonOnClikListiner();
+		ImageButton start = (ImageButton) findViewById(R.id.play);
+//		ImageButton pause = (ImageButton) findViewById(R.id.pause);
+		//start.setOnClickListener(buttonOnClikListinero);
+		//pause.setOnClickListener(buttonOnClikListinero);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		if (mediaPlayer.isPlaying()) {
+			mediaPlayer.stop();
+		}
+		mediaPlayer.release();
+		// Activity销毁时停止播放，释放资源。不做这个操作，即使退出还是能听到视频播放的声音
+	}
+
+	private final class ButtonOnClikListiner implements View.OnClickListener {
+		@Override
+		public void onClick(View v) {
+			if (Environment.getExternalStorageState() == Environment.MEDIA_UNMOUNTED) {
+				Toast.makeText(HomeActivity.this, "sd卡不存在", Toast.LENGTH_SHORT)
+						.show();
+				return;
+			}
+			filename = "2793299.mp4";
+			switch (v.getId()) {
+			case R.id.play:
+				play();
+				break;
+			case R.id.pause:
+				if (mediaPlayer.isPlaying()) {
+					mediaPlayer.pause();
+				} else {
+					mediaPlayer.start();
+				}
+				break;
+
+			}
+		}
+	}
+
+	private void play() {
+		try {
+			File file = new File(Environment.getExternalStorageDirectory(),
+					filename);
+			mediaPlayer.reset();// 重置为初始状态
+			mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);// 设置音乐流的类型
+			mediaPlayer.setDisplay(surfaceView.getHolder());// 设置video影片以surfaceviewholder播放
+			mediaPlayer.setDataSource(file.getAbsolutePath());// 设置路径
+			mediaPlayer.prepare();// 缓冲
+			mediaPlayer.start();// 播放
+		} catch (Exception e) {
+			Log.e(TAG, e.toString());
+			e.printStackTrace();
+		}
+	}
+
+	private final class SurceCallBack implements SurfaceHolder.Callback {
+		/**
+		 * 画面修改
+		 */
+		@Override
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,
+				int height) {
+			// TODO Auto-generated method stub
+
+		}
+
+		/**
+		 * 画面创建
+		 */
+		@Override
+		public void surfaceCreated(SurfaceHolder holder) {
+			if (prosition > 0 && filename != null) {
+				play();
+				mediaPlayer.seekTo(prosition);
+				prosition = 0;
+			}
+
+		}
+
+		/**
+		 * 画面销毁
+		 */
+		@Override
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			if (mediaPlayer.isPlaying()) {
+				prosition = mediaPlayer.getCurrentPosition();
+				mediaPlayer.stop();
+			}
+		}
 	}
 }
---
+++ 金牛山木栈道/src/com/xkx/yjxm/service/AudioService.java
@@ -0,0 +1,134 @@
+package com.xkx.yjxm.service;
+
+import java.io.IOException;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.IBinder;
+import android.view.SurfaceView;
+
+public class AudioService extends Service {
+	private MediaPlayer mediaPlayer;
+	private SurfaceView surfaceView;
+	
+	// 是不是已经导入了资源
+	private boolean isFrist;
+
+	public AudioService() {
+	}
+
+	@Override
+	public void onCreate() {
+		mediaPlayer = new MediaPlayer();
+		super.onCreate();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mediaPlayer.release();
+	}
+
+	@Override
+	public boolean onUnbind(Intent intent) {
+		mediaPlayer.release();
+		return super.onUnbind(intent);
+	}
+
+	public void play(Uri uri) {
+		try {
+			mediaPlayer.reset();
+			mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);// 设置音乐流的类型
+			mediaPlayer.setDisplay(surfaceView.getHolder());// 设置video影片以surfaceviewholder播放
+			mediaPlayer.setDataSource(this, uri);
+			mediaPlayer.setVolume(1, 1);
+			mediaPlayer.prepareAsync();
+			mediaPlayer.setOnPreparedListener(new OnPreparedListener() {
+
+				@Override
+				public void onPrepared(MediaPlayer mp) {
+					mediaPlayer.start();
+					isFrist = true;
+				}
+			});
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		mediaPlayer.setOnCompletionListener(new OnCompletionListener() {
+
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				listener.onPlayComplete();
+			}
+		});
+	}
+
+	public class AudioBinder extends Binder {
+		public void setOnPlayCompleteListener(OnPlayCompleteListener lis) {
+			listener = lis;
+		}
+
+		// 初始化并播放
+		public void audioPlay(SurfaceView surfaceView,Uri uri) {
+			play(uri);
+		}
+
+		// 开始播放
+		public void audioStart() {
+			mediaPlayer.start();
+		}
+
+		// 暂停播放
+		public void audioPause() {
+			mediaPlayer.pause();
+			// 设置为true后，暂停服务后不会被kill掉
+			stopForeground(true);
+		}
+
+		// 停止播放
+		public void audioStop() {
+			mediaPlayer.stop();
+		}
+
+		// 是否在播放
+		public boolean audioIsPlaying() {
+			return mediaPlayer.isPlaying();
+		}
+
+		// 是否已经导入资源
+		public boolean audioIsFrist() {
+			return isFrist;
+		}
+
+		// 播放下一个
+		public void audioPlayNext(Uri uri) {
+			isFrist = false;
+			play(uri);
+		}
+	}
+
+	private OnPlayCompleteListener listener;
+
+	AudioBinder binder = new AudioBinder();
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return binder;
+	}
+
+	public interface OnPlayCompleteListener {
+		public void onPlayComplete();
+	}
+}
