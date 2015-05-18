84810cb7debb9215ac42a4ad76616bb8b685a3ab
余华柱 <yuhuazhu@live.com>
2015-05-18 16:50:50 +0800

    add:所有Activity的父类,此类实现语言切换的功能,ztb.

---
+++ 遇见厦门/src/com/xkx/yjxm/activity/BaseActivity.java
@@ -0,0 +1,42 @@
+package com.xkx.yjxm.activity;
+
+import java.util.Locale;
+
+import com.xkx.yjxm.utils.PreferenceUtil;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class BaseActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// 初始化PreferenceUtil
+		PreferenceUtil.init(this);
+		// 根据上次的语言设置，重新设置语言
+		switchLanguage(PreferenceUtil.getString("language", "zh"));
+	}
+
+	/**
+	 * 切换界面语言
+	 * 
+	 * @param language
+	 *            指定显示的语言代码
+	 */
+	private void switchLanguage(String language) {
+		// 设置应用语言类型
+		Resources resources = getResources();
+		Configuration config = resources.getConfiguration();
+		DisplayMetrics dm = resources.getDisplayMetrics();
+		if (language.equalsIgnoreCase("en")) {
+			config.locale = Locale.ENGLISH;
+		} else if (language.equalsIgnoreCase("zh")) {
+			config.locale = Locale.SIMPLIFIED_CHINESE;
+		}
+		resources.updateConfiguration(config, dm);
+	}
+}
