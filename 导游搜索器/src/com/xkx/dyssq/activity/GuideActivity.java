2d0bf28cde042bbd12abfb39793824c5dd5b62e6
余华柱 <yuhuazhu@live.com>
2015-05-27 20:45:43 +0800

    yhz:更新导游信息

--- 导游搜索器/src/com/xkx/dyssq/activity/GuideActivity.java
+++ 导游搜索器/src/com/xkx/dyssq/activity/GuideActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
@@ -19,10 +20,14 @@ public class GuideActivity extends Activity implements OnClickListener {
 	private Button button3;
 	GradientDrawable drawable;
 	private RelativeLayout backlay;
+	private RelativeLayout daolayout;
+	private RelativeLayout xclayout;
+	private RelativeLayout yklayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_guide);
 
 		initUI();
@@ -30,6 +35,9 @@ public class GuideActivity extends Activity implements OnClickListener {
 
 	private void initUI() {
 		backlay= (RelativeLayout) findViewById(R.id.backlay);
+		daolayout= (RelativeLayout) findViewById(R.id.daolayout);
+		xclayout= (RelativeLayout) findViewById(R.id.xclayout);
+		yklayout= (RelativeLayout) findViewById(R.id.yklayout);
 		button1= (Button) findViewById(R.id.button1);
 		button2 = (Button) findViewById(R.id.button2);
 		button3 = (Button) findViewById(R.id.button3);
@@ -58,6 +66,9 @@ public class GuideActivity extends Activity implements OnClickListener {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
 		case R.id.button1:
+			daolayout.setVisibility(View.VISIBLE);
+			xclayout.setVisibility(View.GONE);
+			yklayout.setVisibility(View.GONE);
 			button1.setBackgroundColor(Color.parseColor("#ff4081"));
 			button1.setTextColor(Color.parseColor("#ffffff"));
 			button2.setBackgroundDrawable(drawable); // 设置背景（效果就是有边框及底色）
@@ -66,6 +77,9 @@ public class GuideActivity extends Activity implements OnClickListener {
 			button3.setTextColor(Color.parseColor("#ff4081"));
 			break;
 		case R.id.button2:
+			daolayout.setVisibility(View.GONE);
+			xclayout.setVisibility(View.VISIBLE);
+			yklayout.setVisibility(View.GONE);
 			button2.setBackgroundColor(Color.parseColor("#ff4081"));
 			button2.setTextColor(Color.parseColor("#ffffff"));
 			button1.setBackgroundDrawable(drawable); // 设置背景（效果就是有边框及底色）
@@ -74,6 +88,9 @@ public class GuideActivity extends Activity implements OnClickListener {
 			button3.setTextColor(Color.parseColor("#ff4081"));
 			break;
 		case R.id.button3:
+			daolayout.setVisibility(View.GONE);
+			xclayout.setVisibility(View.GONE);
+			yklayout.setVisibility(View.VISIBLE);
 			button3.setBackgroundColor(Color.parseColor("#ff4081"));
 			button3.setTextColor(Color.parseColor("#ffffff"));
 			button1.setBackgroundDrawable(drawable); // 设置背景（效果就是有边框及底色）
