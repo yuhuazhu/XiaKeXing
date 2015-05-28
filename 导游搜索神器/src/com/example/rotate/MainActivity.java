a71b2656ccdcce5b741414cf4471433a910ff995
余华柱 <yuhuazhu@live.com>
2015-05-28 09:19:58 +0800

    修改代码,ztb

--- 导游搜索神器/src/com/example/rotate/MainActivity.java
+++ 导游搜索神器/src/com/example/rotate/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.rotate;
 
+import java.util.Comparator;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationSet;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.brtbeacon.sdk.BRTBeacon;
+import com.example.rotate.BleScanService.BleBinder;
+import com.example.rotate.BleScanService.OnBleScanListener;
 
 public class MainActivity extends Activity implements OnClickListener {
 
@@ -26,14 +37,66 @@ public class MainActivity extends Activity implements OnClickListener {
 	private boolean isStart;
 	private boolean isBack;
 	private TextView textView1;
-	private CircleImageView img_zhang;
-	private CircleImageView img_li;
-	private CircleImageView img_liu;
-	private CircleImageView img_zhou;
 	private RelativeLayout daolayout;
-	private CircleImageView img_head;
 	private ImageButton imageButton1;
 	private RelativeLayout headlay;
+
+	private BleBinder bleBinder;
+
+	private ServiceConnection conn = new ServiceConnection() {
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			Log.e("scan", "onServiceDisconnected()");
+		}
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			Log.e("scan", "onServiceConnected");
+			bleBinder = (BleBinder) service;
+
+			bleBinder.setRegion(null);// 空代表扫描所有
+			bleBinder.setOnBleScanListener(new OnBleScanListener() {
+
+				@Override
+				public void onPeriodScan(List<BRTBeacon> scanResultList) {
+					isStart = false;
+					m_im.clearAnimation();
+					m_im.setVisibility(View.INVISIBLE);
+					imgbtnScan.setBackgroundResource(R.drawable.bt1);
+					processScanResult(scanResultList);
+				}
+
+				@Override
+				public void onNearBleChanged(BRTBeacon oriBeacon,
+						BRTBeacon desBeacon) {
+
+				}
+
+				@Override
+				public void onNearBeacon(BRTBeacon brtBeacon) {
+
+				}
+			});
+		}
+	};
+
+	/**
+	 * 最多导游数
+	 */
+	int maxDrawCount = 4;
+
+	Comparator<? super BRTBeacon> rssiComparator = new Comparator<BRTBeacon>() {
+
+		@Override
+		public int compare(BRTBeacon lhs, BRTBeacon rhs) {
+			return rhs.rssi - lhs.rssi;
+		}
+	};
+
+	private void processScanResult(List<BRTBeacon> scanResultList) {
+		drawBeacons(4);
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,55 +106,85 @@ public class MainActivity extends Activity implements OnClickListener {
 		initUI();
 	}
 
-	public void initUI() {
-		headlay = (RelativeLayout) findViewById(R.id.headlay);
-		imageButton1= (ImageButton) findViewById(R.id.imageButton1);
-		CircleImageView cr = new CircleImageView(this);
-		android.widget.RelativeLayout.LayoutParams lp = new android.widget.RelativeLayout.LayoutParams(
-				80, 80);
-		lp.rightMargin = 40;
-		lp.topMargin = 400;
-		lp.addRule(RelativeLayout.LEFT_OF,imageButton1.getId());
-		cr.setLayoutParams(lp);
-		cr.setBackgroundResource(R.drawable.img_zhang);
-		headlay.addView(cr);
-		// <com.example.rotate.CircleImageView
-		// android:id="@+id/img_zhang"
-		// android:layout_width="40dp"
-		// android:layout_height="40dp"
-		// android:layout_marginRight="40dp"
-		// android:layout_toLeftOf="@+id/imageButton1"
-		// android:layout_marginTop="130dp"
-		//
-		// android:background="@drawable/img_zhang" />
+	int[] heads = new int[] { R.drawable.img_li, R.drawable.img_liu,
+			R.drawable.img_zhang, R.drawable.img_zhou };
 
+	int[] rights = new int[] { 50, 500, -300, -500 };
+	int[] tops = new int[] { 75, -140, 500, 200 };
+	CircleImageView[] cr;
+	private ImageButton imgbtnScan;
+
+	private void drawBeacons(int count) {
+		headlay = (RelativeLayout) findViewById(R.id.headlay);
+		imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+		cr = new CircleImageView[4];
+
+		for (int i = 0; i < count; i++) {
+			RelativeLayout.LayoutParams lp = new android.widget.RelativeLayout.LayoutParams(
+					100, 100);
+			cr[i] = new CircleImageView(this);
+			lp.rightMargin = rights[i];
+			lp.topMargin = tops[i];
+			lp.addRule(RelativeLayout.LEFT_OF, imageButton1.getId());
+			if (i % 2 == 0) {
+				lp.addRule(RelativeLayout.ABOVE, imageButton1.getId());
+			} else {
+				lp.addRule(RelativeLayout.BELOW, imageButton1.getId());
+			}
+			cr[i].setLayoutParams(lp);
+			cr[i].setBackgroundResource(heads[i]);
+			final int index = i;
+			cr[i].setOnClickListener(new OnClickListener() {
+				boolean isLarge = false;
+
+				@Override
+				public void onClick(View v) {
+					isLarge = !isLarge;
+					if (isLarge) {
+						// 放大头像
+						Animation scaleAnimation = new ScaleAnimation(1f, 2.0f,
+								1f, 2.0f);
+						scaleAnimation.setDuration(1000);
+						scaleAnimation.setFillAfter(false);
+						cr[index].startAnimation(scaleAnimation);
+						// TODO 显示信息
+					} else {
+						// 还原
+						Animation scaleAnimation = new ScaleAnimation(2f, 1f,
+								2f, 1f);
+						scaleAnimation.setDuration(500);
+						scaleAnimation.setFillAfter(true);
+						cr[index].startAnimation(scaleAnimation);
+						// TODO 隐藏信息
+					}
+				}
+			});
+			headlay.addView(cr[i], lp);
+		}
+	}
+
+	public void initUI() {
+		imgbtnScan = (ImageButton) findViewById(R.id.button1);
 		m_im = (ImageView) findViewById(R.id.imageView1);
 		m_im.setVisibility(View.INVISIBLE);
 		textView1 = (TextView) findViewById(R.id.textView1);
 		daolayout = (RelativeLayout) findViewById(R.id.daolayout);
-		img_head = (CircleImageView) findViewById(R.id.img_head);
-		img_zhang = (CircleImageView) findViewById(R.id.img_zhang);
-		img_li = (CircleImageView) findViewById(R.id.img_li);
-		img_liu = (CircleImageView) findViewById(R.id.img_liu);
-		img_zhou = (CircleImageView) findViewById(R.id.img_zhou);
-		img_zhang.setOnClickListener(this);
-		img_li.setOnClickListener(this);
-		img_liu.setOnClickListener(this);
-		img_zhou.setOnClickListener(this);
 	}
 
-	public void startAnimation(View v) {
+	public void process(View v) {
 		// 更改状态
 		isStart = !isStart;
 		v.setBackgroundResource(isStart ? R.drawable.bt2 : R.drawable.bt1);
 		final ImageButton button = (ImageButton) v;
 
 		if (isStart) {
+			// 开启蓝牙扫描
+			Intent service = new Intent(MainActivity.this, BleScanService.class);
+			bindService(service, conn, BIND_AUTO_CREATE);
 			AnimationSet animationSet = new AnimationSet(true);
-			int width = m_im.getWidth();
-			int height = m_im.getHeight();
-			RotateAnimation animation = new RotateAnimation(0f, 360f * 10, 0, 0);
-			animation.setDuration(5000);// 设置动画持续时间
+			RotateAnimation animation = new RotateAnimation(0f, 360f, 0, 0);
+			animation.setRepeatCount(3);
+			animation.setDuration(4000);// 设置动画持续时间
 			animationSet.addAnimation(animation);
 			animationSet.setInterpolator(new LinearInterpolator());
 			m_im.startAnimation(animationSet);
@@ -107,7 +200,6 @@ public class MainActivity extends Activity implements OnClickListener {
 				@Override
 				public void onAnimationRepeat(Animation animation) {
 					// TODO Auto-generated method stub
-
 				}
 
 				@Override
@@ -116,14 +208,17 @@ public class MainActivity extends Activity implements OnClickListener {
 					m_im.clearAnimation();
 					m_im.setVisibility(View.INVISIBLE);
 					button.setBackgroundResource(R.drawable.bt1);
-					textView1.setText("已搜索到附近4位导游,点击头像查看信息");
-					img_zhang.setVisibility(View.VISIBLE);
-					img_li.setVisibility(View.VISIBLE);
-					img_liu.setVisibility(View.VISIBLE);
-					img_zhou.setVisibility(View.VISIBLE);
 				}
 			});
 		} else {
+			// 解绑服务,取消扫描.
+			unbindService(conn);
+			// 清除画上去的头像
+			if (cr != null) {
+				for (int i = 0; i < cr.length; i++) {
+					headlay.removeView(cr[i]);
+				}
+			}
 			m_im.clearAnimation();
 			m_im.setVisibility(View.INVISIBLE);
 		}
@@ -159,42 +254,7 @@ public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
-		LayoutParams para;
-
-		// TODO Auto-generated method stub
 		switch (v.getId()) {
-		case R.id.img_zhang:
-			daolayout.setVisibility(View.VISIBLE);
-			img_head.setBackgroundResource(R.drawable.img_zhang);
-			para = img_zhang.getLayoutParams();
-			para.height = 70;
-			para.width = 70;
-			img_zhang.setLayoutParams(para);
-			break;
-		case R.id.img_li:
-			daolayout.setVisibility(View.VISIBLE);
-			img_head.setBackgroundResource(R.drawable.img_li);
-			para = img_li.getLayoutParams();
-			para.height = 70;
-			para.width = 70;
-			img_li.setLayoutParams(para);
-			break;
-		case R.id.img_liu:
-			daolayout.setVisibility(View.VISIBLE);
-			img_head.setBackgroundResource(R.drawable.img_liu);
-			para = img_liu.getLayoutParams();
-			para.height = 70;
-			para.width = 70;
-			img_liu.setLayoutParams(para);
-			break;
-		case R.id.img_zhou:
-			daolayout.setVisibility(View.VISIBLE);
-			img_head.setBackgroundResource(R.drawable.img_zhou);
-			para = img_zhou.getLayoutParams();
-			para.height = 70;
-			para.width = 70;
-			img_zhou.setLayoutParams(para);
-			break;
 		default:
 			break;
 		}
