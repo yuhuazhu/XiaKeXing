f3a4b3fde074f7cafa04c28c0fcd66ad63efb314
余华柱 <yuhuazhu@live.com>
2015-05-29 09:20:22 +0800

    modify:format code and add bluetooth trigger.[ztb]

--- 导游搜索器/src/com/xkx/dyssq/activity/MainActivity.java
+++ 导游搜索器/src/com/xkx/dyssq/activity/MainActivity.java
@@ -1,30 +1,39 @@
 package com.xkx.dyssq.activity;
 
-import com.xkx.dyssq.R;
+import java.util.List;
 
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.os.Vibrator;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationSet;
 import android.view.animation.LinearInterpolator;
-import android.view.animation.Animation.AnimationListener;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.brtbeacon.sdk.BRTBeacon;
+import com.xkx.dyssq.R;
+import com.xkx.dyssq.activity.BleScanService.BleBinder;
+import com.xkx.dyssq.activity.BleScanService.OnBleScanListener;
 
 public class MainActivity extends Activity {
 
@@ -38,46 +47,62 @@ public class MainActivity extends Activity {
 	private SensorManager sensorManager;
 	private final int MSG_SENSOR_SHAKE = 10;
 	private Vibrator vibrator;
-	
+	private BleBinder bleBinder;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE); 
-        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE); 
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 		m_im1 = (ImageView) findViewById(R.id.imageView1);
 		m_im2 = (ImageView) findViewById(R.id.imageView2);
+		m_im2.setEnabled(false);
 		m_ll = (LinearLayout) findViewById(R.id.linearLayout);
 		m_ll.setVisibility(View.INVISIBLE);
 		m_ll.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
-			public void onClick(View v) 
-			{
+			public void onClick(View v) {
 				Intent intent = null;
 				intent = new Intent(MainActivity.this, GuideActivity.class);
 				startActivity(intent);
 			}
 		});
 	}
-	
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+		adapter.enable();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		// 关闭蓝牙
+		BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+		adapter.disable();
+	}
+
 	@Override
 	protected void onPause() {
 		super.onPause();
-		//注销
+		// 注销
 		sensorManager.unregisterListener(sensorEventListener);
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
-		//注册
+		// 注册
 		sensorManager.registerListener(sensorEventListener,
 				sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
 				SensorManager.SENSOR_DELAY_NORMAL);
 	}
-	
+
 	/**
 	 * 重力感应监听
 	 */
@@ -106,7 +131,7 @@ public class MainActivity extends Activity {
 
 		@Override
 		public void onAccuracyChanged(Sensor sensor, int accuracy) {
-			
+
 		}
 	};
 
@@ -126,143 +151,164 @@ public class MainActivity extends Activity {
 		}
 
 		private void processAfterShake() {
-			// BluetoothDevice device = bleService.getProximityBleDevice();
-			// BRTBeacon beacon = bleBinder.getProximityBeacon();
-			// if (beacon != null) {
-			// trigger(beacon);
-			// }
-			// Intent intent = null;
-			// intent = new Intent(MainActivity.this, GuideActivity.class);
-			// startActivity(intent);
+			Intent service = new Intent(MainActivity.this, BleScanService.class);
+			bindService(service, conn, BIND_AUTO_CREATE);
 			startPeople(m_im2);
 		}
 	};
 
-	public void startPeople(View v)
-	{
-		//更改状态
+	private ServiceConnection conn = new ServiceConnection() {
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			Log.e("scan", "onServiceDisconnected()");
+		}
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			Log.e("scan", "onServiceConnected");
+			bleBinder = (BleBinder) service;
+
+			// bleBinder.setRegion("E2C56DB5-DFFB-48D2-B060-D0F5A71096E0");//
+			// 空代表扫描所有
+			bleBinder.setOnBleScanListener(new OnBleScanListener() {
+
+				public void onPeriodScan(List<BRTBeacon> scanResultList) {
+					unbindService(conn);
+				}
+
+				public void onNearBleChanged(BRTBeacon oriBeacon,
+						BRTBeacon desBeacon) {
+
+				}
+
+				public void onNearBeacon(BRTBeacon brtBeacon) {
+					Log.e("beacon", brtBeacon.macAddress);
+					if (brtBeacon == null) {
+						Toast.makeText(MainActivity.this, "没有摇到噢,再摇一下",
+								Toast.LENGTH_LONG).show();
+					} else {
+						startGuide();
+					}
+				}
+			});
+		}
+	};
+
+	public void startPeople(View v) {
+		// 更改状态
 		isStart = !isStart;
 		v.setClickable(false);
 		m_ll.setVisibility(View.INVISIBLE);
 		final ImageView im = (ImageView) v;
-		
-		if(isStart)
-		{
+
+		if (isStart) {
 			m_im1.setBackgroundResource(R.drawable.shake2);
 			m_im2.setBackgroundResource(R.drawable.people2);
-			
+
 			final AnimationSet animationSet = new AnimationSet(true);
-			
+
 			TranslateAnimation animation1 = new TranslateAnimation(0, 30, 0, 0);
 			TranslateAnimation animation2 = new TranslateAnimation(0, -60, 0, 0);
 			TranslateAnimation animation3 = new TranslateAnimation(0, 60, 0, 0);
 			TranslateAnimation animation4 = new TranslateAnimation(0, -30, 0, 0);
-			
-			//设置动画持续时间 
+
+			// 设置动画持续时间
 			animation1.setDuration(70);
 			animation2.setDuration(140);
 			animation3.setDuration(140);
 			animation4.setDuration(70);
-			
-			//延迟播放
-//			animation1.setStartOffset();  
-			animation2.setStartOffset(70);  
-			animation3.setStartOffset(140);  
-			animation4.setStartOffset(140);  
-			
+
+			// 延迟播放
+			// animation1.setStartOffset();
+			animation2.setStartOffset(70);
+			animation3.setStartOffset(140);
+			animation4.setStartOffset(140);
+
 			animationSet.addAnimation(animation1);
 			animationSet.addAnimation(animation2);
 			animationSet.addAnimation(animation3);
 			animationSet.addAnimation(animation4);
-			
+
 			animationSet.setInterpolator(new LinearInterpolator());
 			m_im2.startAnimation(animationSet);
 			animationSet.setAnimationListener(new AnimationListener() {
-				
+
 				@Override
 				public void onAnimationStart(Animation animation) {
-					
+
 				}
-				
+
 				@Override
 				public void onAnimationRepeat(Animation animation) {
-					
+
 				}
-				
+
 				@Override
-				public void onAnimationEnd(Animation animation)
-				{
-//					m_im2.clearAnimation();
-					if(animationCount-- != 0)
-					{
+				public void onAnimationEnd(Animation animation) {
+					// m_im2.clearAnimation();
+					if (animationCount-- != 0) {
 						m_im2.startAnimation(animationSet);
-					}
-					else
-					{
+					} else {
 						m_im2.clearAnimation();
 						isStart = false;
 						m_im1.setBackgroundResource(R.drawable.shake1);
 						m_im2.setBackgroundResource(R.drawable.people1);
 						m_im2.setClickable(true);
 						animationCount = 5;
-						startGuide();
+						// startGuide();
 					}
 				}
 			});
 		}
 	}
-	
-	public void startGuide()
-	{
+
+	public void startGuide() {
 		m_ll.setVisibility(View.VISIBLE);
 		final AnimationSet animationSet = new AnimationSet(true);
-		
+
 		TranslateAnimation animation1 = new TranslateAnimation(0, 60, 0, 0);
 		TranslateAnimation animation2 = new TranslateAnimation(0, -90, 0, 0);
 		TranslateAnimation animation3 = new TranslateAnimation(0, 90, 0, 0);
 		TranslateAnimation animation4 = new TranslateAnimation(0, -60, 0, 0);
-		
-		//设置动画持续时间 
+
+		// 设置动画持续时间
 		animation1.setDuration(40);
 		animation2.setDuration(80);
 		animation3.setDuration(80);
 		animation4.setDuration(40);
-		
-		//延迟播放
-//			animation1.setStartOffset();  
-		animation2.setStartOffset(40);  
-		animation3.setStartOffset(80);  
-		animation4.setStartOffset(80);  
-		
+
+		// 延迟播放
+		// animation1.setStartOffset();
+		animation2.setStartOffset(40);
+		animation3.setStartOffset(80);
+		animation4.setStartOffset(80);
+
 		animationSet.addAnimation(animation1);
 		animationSet.addAnimation(animation2);
 		animationSet.addAnimation(animation3);
 		animationSet.addAnimation(animation4);
-			
+
 		animationSet.setInterpolator(new LinearInterpolator());
 		m_ll.startAnimation(animationSet);
 		animationSet.setAnimationListener(new AnimationListener() {
-			
+
 			@Override
 			public void onAnimationStart(Animation animation) {
-				
+
 			}
-			
+
 			@Override
 			public void onAnimationRepeat(Animation animation) {
-				
+
 			}
-			
+
 			@Override
-			public void onAnimationEnd(Animation animation)
-			{
-//					m_im2.clearAnimation();
-				if(animationCount2-- != 0)
-				{
+			public void onAnimationEnd(Animation animation) {
+				// m_im2.clearAnimation();
+				if (animationCount2-- != 0) {
 					m_ll.startAnimation(animationSet);
-				}
-				else
-				{
+				} else {
 					m_ll.clearAnimation();
 					isStart = false;
 					animationCount2 = 3;
@@ -270,33 +316,31 @@ public class MainActivity extends Activity {
 			}
 		});
 	}
-	
-	//跳转页面
-	public void startIn(View v) 
-	{
+
+	// 跳转页面
+	public void startIn(View v) {
 		Intent intent = null;
 		intent = new Intent(this, GuideActivity.class);
 		startActivity(intent);
 	}
-	
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if(event.getKeyCode() == KeyEvent.KEYCODE_BACK)
-		{
-			if(isBack){
+		if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+			if (isBack) {
 				finish();
 				System.exit(0);
-			}else{
+			} else {
 				isBack = true;
-				Toast.makeText(MainActivity.this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+				Toast.makeText(MainActivity.this, "再按一次退出程序",
+						Toast.LENGTH_SHORT).show();
 				m_im1.postDelayed(new Runnable() {
-					public void run() 
-					{
+					public void run() {
 						isBack = false;
 					}
 				}, 2000);
 			}
-			
+
 		}
 		return false;
 	}
