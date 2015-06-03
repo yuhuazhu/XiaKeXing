ca89c36f6b6c54db6b303fccdd4446886723232c
余华柱 <yuhuazhu@live.com>
2015-06-03 09:45:15 +0800

    //加入下载功能。

--- 遇见厦门/src/com/xkx/yjxm/activity/RouteActivity.java
+++ 遇见厦门/src/com/xkx/yjxm/activity/RouteActivity.java
@@ -69,7 +69,7 @@ import com.xkx.yjxm.db.MySqlite;
 import com.xkx.yjxm.utils.HttpUtil;
 
 //导游路线
-public class RouteActivity extends Activity implements OnClickListener {
+public class RouteActivity extends BaseActivity implements OnClickListener {
 	private ImageButton btnlist;
 	private ImageButton btndlist;
 	private ImageButton btnback;
@@ -146,7 +146,7 @@ public class RouteActivity extends Activity implements OnClickListener {
 			random2 = new Random(nextInt);
 			times = random2.nextInt(10) + 5;
 
-			time[i] = "浏览时长：" + times + "分钟";
+			time[i] = getResources().getString(R.string.R_time1) + times + getResources().getString(R.string.R_time2);
 		}
 		for (int i = 0; i < img.length; i++) {
 			distances += 10;
@@ -840,7 +840,7 @@ public class RouteActivity extends Activity implements OnClickListener {
 			// "drawable://" + (Integer) list.get(position).get("img"),
 			// holder.imageView1, options);
 
-			holder.txttitle.setText(title[position]);
+			holder.txttitle.setText(R.string.route_title0 + position);
 			holder.txttime.setText(time[position]);
 			holder.txtdistance.setText(distance[position]);
 
