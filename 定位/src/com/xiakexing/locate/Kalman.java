61a4e97137abcce6943bf54bdf10f0f52dd54104
余华柱 <yuhuazhu@live.com>
2015-06-16 09:35:59 +0800

    add:卡尔曼算法.[ztb]

---
+++ 定位/src/com/xiakexing/locate/Kalman.java
@@ -0,0 +1,29 @@
+package com.xiakexing.locate;
+
+/**
+ * <pre>
+ * 根据卡尔曼滤波算法写的类，用于保存每个时刻的数据信息。
+ * see at http://blog.chinaunix.net/uid-26694208-id-3184442.html
+ * </pre>
+ * 
+ * @author Administrator
+ * 
+ */
+public class Kalman {
+	/** 测量值 */
+	int valMeas;
+	/** 估计值 */
+	int valEsti;
+	/** 最优值 */
+	int valOpti;
+	/** 估计值不确定度 */
+	int uncerEsti;
+	/** 测量值不确定度 */
+	int uncerMeas;
+	/** 估计值偏差 */
+	double devEsti;
+	/** 测量值偏差 */
+	double devMeas;
+	/** 最优值偏差 */
+	double devOpti;
+}
