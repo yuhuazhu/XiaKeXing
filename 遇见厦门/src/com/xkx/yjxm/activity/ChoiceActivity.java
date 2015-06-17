3ea535d4b0a68e4ae4c9db66582354918b061985
余华柱 <yuhuazhu@live.com>
2015-06-17 16:53:47 +0800

    //修改列表点击样式。

--- 遇见厦门/src/com/xkx/yjxm/activity/ChoiceActivity.java
+++ 遇见厦门/src/com/xkx/yjxm/activity/ChoiceActivity.java
@@ -1,35 +1,43 @@
 package com.xkx.yjxm.activity;
 
-import com.xkx.yjxm.R;
-import com.xkx.yjxm.R.layout;
-import com.xkx.yjxm.R.menu;
-
-import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.Window;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.xkx.yjxm.R;
+
 //选择界面
-public class ChoiceActivity extends BaseActivity {
+public class ChoiceActivity extends BaseActivity implements OnTouchListener,
+OnGestureListener {
+GestureDetector mGestureDetector;
 
 	private boolean isList;
 	private ImageButton imageButton3;
 	private ImageButton imageButton2;
+
+	private static final int FLING_MIN_DISTANCE = 300;
+	private static final int FLING_MIN_VELOCITY = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_choice);
+		mGestureDetector = new GestureDetector(this);
 		// initUI();
-
+		RelativeLayout ll = (RelativeLayout) findViewById(R.id.relay01);
+		ll.setOnTouchListener(this);
+		ll.setLongClickable(true);  
 		Intent intent = getIntent();
 		isList = intent.getBooleanExtra("列表", true);
 	}
@@ -103,6 +111,62 @@ public class ChoiceActivity extends BaseActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.choice, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onDown(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onLongPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		// TODO Auto-generated method stub
+		if (e1.getX() - e2.getX() > FLING_MIN_DISTANCE
+				&& Math.abs(velocityX) > FLING_MIN_VELOCITY) {
+			// Fling left
+			Toast.makeText(this, "向左手势 ", Toast.LENGTH_SHORT).show();
+		} else if (e2.getX() - e1.getX() > FLING_MIN_DISTANCE
+				&& Math.abs(velocityX) > FLING_MIN_VELOCITY) {
+			// Fling right
+			finish();
+		}
+		return false;
+
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		// TODO Auto-generated method stub
+
+		Log.i("touch", "touch");
+		return mGestureDetector.onTouchEvent(event);
 	}
 
 }
--- 遇见厦门/src/com/xkx/yjxm/activity/MainActivity.java
+++ 遇见厦门/src/com/xkx/yjxm/activity/MainActivity.java
@@ -3,34 +3,38 @@ package com.xkx.yjxm.activity;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import com.xkx.yjxm.R;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
 import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.Window;
-import android.view.View.OnClickListener;
-import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.Toast;
-import android.widget.RelativeLayout.LayoutParams;
 
 import com.xkx.yjxm.R;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements OnTouchListener,
+		OnGestureListener {
+	GestureDetector mGestureDetector;
 	private boolean isExit;
 	private ImageView im_paopao;
 	private ImageView im_paopao_x;
+	private static final int FLING_MIN_DISTANCE = 300;
+	private static final int FLING_MIN_VELOCITY = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +42,11 @@ public class MainActivity extends BaseActivity {
 		// 移除ActionBar，在setContent之前调用下面这句，保证没有ActionBar
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		mGestureDetector = new GestureDetector(this);
 		im_paopao = (ImageView) findViewById(R.id.img_paopao);
+		FrameLayout ll = (FrameLayout) findViewById(R.id.ll);
+		ll.setOnTouchListener(this);
+		ll.setLongClickable(true);    
 		im_paopao_x = (ImageView) findViewById(R.id.img_paopao_x);
 		StartPaoPao();
 	}
@@ -125,7 +133,7 @@ public class MainActivity extends BaseActivity {
 
 			@Override
 			public void onAnimationStart(Animation animation) {
-				//移动到屏幕外，这样就不会出现图片跳动
+				// 移动到屏幕外，这样就不会出现图片跳动
 				RelativeLayout.LayoutParams param = new RelativeLayout.LayoutParams(
 						LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 				param.setMargins(-100, 0, 0, 0);
@@ -152,7 +160,8 @@ public class MainActivity extends BaseActivity {
 						LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 				param1.setMargins(left, top, 0, 0);
 				im_paopao.setLayoutParams(param1);
-				param2.setMargins(left + im_paopao.getWidth() / 3 * 2, top, 0, 0);
+				param2.setMargins(left + im_paopao.getWidth() / 3 * 2, top, 0,
+						0);
 				im_paopao_x.setLayoutParams(param2);
 				im_paopao_x.setVisibility(View.VISIBLE);
 				im_paopao.setOnClickListener(new OnClickListener() {
@@ -165,10 +174,10 @@ public class MainActivity extends BaseActivity {
 					}
 				});
 				im_paopao_x.setOnClickListener(new OnClickListener() {
-					
+
 					@Override
 					public void onClick(View v) {
-						
+
 						im_paopao.setVisibility(View.INVISIBLE);
 						im_paopao_x.setVisibility(View.INVISIBLE);
 					}
@@ -212,6 +221,78 @@ public class MainActivity extends BaseActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onDown(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		// TODO Auto-generated method stub
+		// float y1 = e1.getY(), y2 = e2.getY();
+		// if (y1 -y2 > 120) {
+		// if (mDirection != SOUTH) {
+		// mNextDirection = NORTH;
+		// }
+		// Log.d(this.getClass().getName(), "To UP" + "(" + y1
+		// + "," + y2 + ")");
+		// return (true);
+		// } else if (y1 - y2 < -120) {
+		// if (mDirection != NORTH) {
+		// mNextDirection = SOUTH;
+		// }
+		// Log.d(this.getClass().getName(), "To Down" + "(" + y1
+		// + "," + y2 + ")");
+		// return (true);
+		// }
+		return false;
+	}
+
+	@Override
+	public void onLongPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		// TODO Auto-generated method stub
+		if (e1.getX() - e2.getX() > FLING_MIN_DISTANCE
+				&& Math.abs(velocityX) > FLING_MIN_VELOCITY) {
+			// Fling left
+			Toast.makeText(this, "向左手势 ", Toast.LENGTH_SHORT).show();
+		} else if (e2.getX() - e1.getX() > FLING_MIN_DISTANCE
+				&& Math.abs(velocityX) > FLING_MIN_VELOCITY) {
+			// Fling right
+			finish();
+		}
+		return false;
+
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		// TODO Auto-generated method stub
+
+		Log.i("touch", "touch");
+		return mGestureDetector.onTouchEvent(event);
 	}
 
 }
